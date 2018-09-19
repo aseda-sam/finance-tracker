6b6be162d3a1705f0d8768d1875a13ff20fe906a
Aseda Sam <aoheneadu23@gmail.com>
2018-09-19 16:54:40 +0000

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,15 @@ Rails.application.routes.draw do
   root 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   
-  get 'my_friends', to: 'users#my_friends'
+  # get 'my_friends', to: 'users#my_friends'
+  get 'my_friends' => 'users#my_friends'
   
   resources :user_stocks, only: [:create, :destroy]
+  resources :user, only: [:show]
+  resources :friendships
+  
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
